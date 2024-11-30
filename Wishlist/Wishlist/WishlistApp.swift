5fbb5b40ee208e2a245d64d8d6d5cc8370b22c94
Oleh Didushok <didushokov@gmail.com>
2024-11-30 13:38:50 +0200

    Setup project

---
+++ Wishlist/Wishlist/WishlistApp.swift
@@ -0,0 +1,17 @@
+//
+//  WishlistApp.swift
+//  Wishlist
+//
+//  Created by Oleh on 30.11.2024.
+//
+
+import SwiftUI
+
+@main
+struct WishlistApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
